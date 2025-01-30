30a80e57a9ed0bb97c63adeba0bc3f76639c93a8
NilsRousseau <nilsrousseau9@gmail.com>
2025-01-30 16:45:29 +0100

    second chart

--- src/charts/first.js
+++ src/charts/first.js
@@ -1,18 +1,22 @@
 const ctx = document.getElementById('firstChart');
 
-const labels = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet"];
 const data = {
-  labels: labels,
-  datasets: [{
-    label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
-    fill: false,
-    borderColor: 'rgb(75, 192, 192)',
-    tension: 0.1
-  }]
-};
+    labels: [
+        'Part des migrants clandestins',
+        'Population native',
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [51200, 204800],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+      ],
+      hoverOffset: 4
+    }]
+  };
 
 new Chart(ctx, {
-    type: 'line',
+    type: 'doughnut',
     data: data,
 });
---
+++ src/charts/second.js
@@ -0,0 +1,39 @@
+const ctx = document.getElementById('secondChart');
+
+const labels = ["Mayotte", "France", "Russie", "Japon", "Mondiale"];
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'Espérance de vie',
+    data: [75.5, 82.3, 73.2, 84.6, 72.6],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+    ],
+    borderWidth: 1
+  }]
+};
+
+const config = {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+};
+
+new Chart(ctx, config);
